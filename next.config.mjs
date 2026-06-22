a292958416414002e4963cd50bf0b60daa6b1130
dascheberg <progdieter@dascheberg.de>
2026-06-22 16:51:41 +0200

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["pdfkit"],
+  },
+};
+
+export default nextConfig;
